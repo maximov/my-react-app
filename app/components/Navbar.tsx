c945f1b0cf6e74c068195cd61c0bef47b5703a62
Egor <maximovkhv@gmail.com>
2025-03-17 18:43:13 +1000

    css для навигации

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="navbar">
             <ul>
                 <li><NavLink to="/">Главная</NavLink></li>
                 <li><NavLink to="/about">О нас</NavLink></li>                
